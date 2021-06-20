fbb879eaa87db9149e9edcd1da9ad8c2ca6db1b6
Lauren Wash <77559166+laurenwmo@users.noreply.github.com>
2021-06-20 14:05:05 +0000

    ?

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -155,6 +155,7 @@ function scorerPrompt() {
 
 
 
+
 function transform(obj) {
     var newPointStructure = {};
     for (pointValue in oldPointStructure) {
@@ -167,13 +168,18 @@ function transform(obj) {
 
 
 
-//{ a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 }
 
 
 
-
-
-let newPointStructure = transform(oldPointStructure);
+let newPointStructure = {
+  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+  2: ['D', 'G'],
+  3: ['B', 'C', 'M', 'P'],
+  4: ['F', 'H', 'V', 'W', 'Y'],
+  5: ['K'],
+  8: ['J', 'X'],
+  10: ['Q', 'Z']
+};
 
 
 
